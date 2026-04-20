28cc73b4f4057bc85686a1bd91c326f005caed96
Federico García Cendra <45184723+xTorettox@users.noreply.github.com>
2026-04-20 02:34:17 +0000

    Cambiar posición de la terminar

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,31 @@ st.set_page_config(page_title="WL Hopper - Sullair Argentina", page_icon="img/fa
 VERDE_SULLAIR = "#008657"
 st.markdown(f"""
     <style>
-    div.stButton > button:first-child {{ background-color: {VERDE_SULLAIR} !important; color: white !important; font-weight: bold; }}
-    .terminal-box {{ background-color: #212529; color: #f8f9fa; font-family: 'Consolas', monospace; font-size: 13px; padding: 15px; border-radius: 5px; height: 535px; overflow-y: auto; border: 1px solid #444; }}
+    /* Forzamos a que las columnas alineen su contenido al fondo */
+    [data-testid="stColumn"] {{
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-end;
+    }}
+
+    div.stButton > button:first-child {{ 
+        background-color: {VERDE_SULLAIR} !important; 
+        color: white !important; 
+        font-weight: bold; 
+    }}
+
+    .terminal-box {{ 
+        background-color: #212529; 
+        color: #f8f9fa; 
+        font-family: 'Consolas', monospace; 
+        font-size: 13px; 
+        padding: 15px; 
+        border-radius: 5px; 
+        height: 535px; /* Mantiene su tamaño */
+        overflow-y: auto; 
+        border: 1px solid #444; 
+    }}
+
     .log-entry {{ margin-bottom: 5px; border-bottom: 1px solid #333; padding-bottom: 2px; }}
     .logo-container {{ display: flex; justify-content: center; align-items: center; flex-direction: column; margin-bottom: 10px; }}
     </style>
