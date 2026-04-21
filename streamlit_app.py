dfea1910910f25aafb81734ab651631554736ac0
Federico García Cendra <45184723+xTorettox@users.noreply.github.com>
2026-04-20 22:49:45 -0300

    Refactor streamlit_app.py for improved structure

--- streamlit_app.py
+++ streamlit_app.py
@@ -98,9 +98,9 @@ if check_password():
                 st.write("🤖") # Placeholder
         with c2:
             st.markdown("""
-            **WL Hopper** es una app diseñada para optimizar la descarga de certificados en PDF desde el sitio de **Worklift**.
+            **WL Hopper** es una app diseñada para optimizar la descarga de certificados desde el sitio de **Worklift**.
             
-            Inspirada en una tarea repetitiva que no quería seguirlo siendo, esta herramienta usa bots de navegación para descargar certificados.
+            Inspirada en una tarea repetitiva que no quería seguirlo siendo, esta herramienta usa bots de navegación para descargar PDFs en segundo plano.
             """)
         
         st.info("🚀 **Misión:** Automatizar y acelerar la tarea de descarga masiva de certificados e informes, y recuperar y estructurar la información de nuestros equipos desde el sitio web de Worklift.")
